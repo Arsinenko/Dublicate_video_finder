fe61cbe6f71a4ae8b69c10a6c5b330730c08583d
jxhrk <183056250+jxhrk@users.noreply.github.com>
2024-09-28 23:14:50 +0300

    Update video_hash.py

--- video_hash.py
+++ video_hash.py
@@ -6,7 +6,7 @@ from converter import hash_to_bytearray, bytearray_to_hash, hash_bytearray_to_ha
 w, h = 100, 200
 
 def get_hash(path_to_file):
-    capture = cv2.VideoCapture(path_to_file)
+    capture = cv2.VideoCapture("videos/video3.mp4")
     fps = int(capture.get(cv2.CAP_PROP_FPS))
     length = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
@@ -19,19 +19,19 @@ def get_hash(path_to_file):
             success, frame = capture.read()
 
             resized_image = cv2.resize(frame, (w, h))
-            #gray = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+            gray = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
 
-            img = Image.fromarray(resized_image)
-            #img_hor = ImageOps.mirror(img)
+            img = Image.fromarray(gray)
+            img_hor = ImageOps.mirror(img)
 
-            #blended_hor = Image.blend(img, img_hor, alpha=0.5)
-            #img_ver = ImageOps.flip(blended_hor)
+            blended_hor = Image.blend(img, img_hor, alpha=0.5)
+            img_ver = ImageOps.flip(blended_hor)
 
-            #blended_ver = Image.blend(blended_hor, img_ver, alpha=0.5)
-            #cropped = blended_ver.crop((0, 0, w / 2, h / 2))
+            blended_ver = Image.blend(blended_hor, img_ver, alpha=0.5)
+            cropped = blended_ver.crop((0, 0, w / 2, h / 2))
 
-            #hash1 = imagehash.phash(img, 16)
-            #result.extend(hash_to_bytearray(hash1))
+            hash1 = imagehash.phash(cropped, 16)
+            result.extend(hash_to_bytearray(hash1))
 
 
     capture.release()
