92f4b9398bef72c684b657a3b05edc48062bd99f
Kim Arsinenko <124343207+Arsinenko@users.noreply.github.com>
2024-09-28 17:43:52 +0300

    update

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, DateTime, String, LargeBi
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.types import UUID
 
+
 Base = declarative_base()
 
 
