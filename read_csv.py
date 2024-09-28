507d7b5aaa323981c2078496ac302c5631262af6
jxhrk <183056250+jxhrk@users.noreply.github.com>
2024-09-28 22:23:16 +0300

    fix names again

--- read_csv.py
+++ read_csv.py
@@ -20,7 +20,7 @@ def add_videos_from_csv():
         for row in rows:
             print(f"{n} из {count_row}")
             #print(f"uuid {row[1]}\nupload {row[0]}\ndupl {row[3]}\nhard {row[5]}")
-            add_video(UUID=uuid.UUID(row[1]), upload_date=row[0], is_duplicate=row[3], is_hard=row[5])
+            add_video(UUID=uuid.UUID(row[1]), upload_date=row[0], is_dupl=row[3], is_hard=row[5])
             n+=1
 
         close_session()
