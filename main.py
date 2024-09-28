a710df1feab37ab8a9470a0b4ab4c01fe40544a5
Kim Arsinenko <124343207+Arsinenko@users.noreply.github.com>
2024-09-28 17:08:45 +0300

    update

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import cv2
-import imagehash
 from PIL import Image, ImageOps
-import asyncio
-from database import DB
+import imagehash
 
-base = DB()
+w, h = 100, 200
 
-w = 100
-h = 200
-
-async def get_hash(path_to_file):
+def get_hash(path_to_file):
     capture = cv2.VideoCapture(path_to_file)
     fps = int(capture.get(cv2.CAP_PROP_FPS))
     length = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -27,39 +22,19 @@ async def get_hash(path_to_file):
 
             img = Image.fromarray(gray)
             img_hor = ImageOps.mirror(img)
+
             blended_hor = Image.blend(img, img_hor, alpha=0.5)
             img_ver = ImageOps.flip(blended_hor)
+
             blended_ver = Image.blend(blended_hor, img_ver, alpha=0.5)
-            cropped = blended_ver.crop((0, 0, w/2, h/2))
-            
+            cropped = blended_ver.crop((0, 0, w / 2, h / 2))
+
             hash1 = imagehash.phash(cropped, 16)
             result.append(hash1)
-            # base.connect()
-            # base.add_video_hash(hash1)
-            #
-
+            print(hash1)
 
     capture.release()
     return result
 
 
-async def main():
-    paths_to_files = ["videos/original.mp4", "videos/dublicate_video.mp4", "videos/video3.mp4"]
-    tasks = []
-
-    for path in paths_to_files:
-        task = asyncio.create_task(get_hash(path))
-        tasks.append(task)
-
-    results = await asyncio.gather(*tasks)
-    return results
-
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    results = loop.run_until_complete(main())
-    print(results)
-
-# for i in range(0,len(data1)):
-#     print(data2[i]-data1[i])
-
+get_hash("https://s3.ritm.media/yappy-db-duplicates/23fac2f2-7f00-48cb-b3ac-aac8caa3b6b4.mp4")
