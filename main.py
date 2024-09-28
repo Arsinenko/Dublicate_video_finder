a589bc9608cc99cff37eedbad419b0f6adeb0ad8
jxhrk <183056250+jxhrk@users.noreply.github.com>
2024-09-28 22:01:18 +0300

    update

--- main.py
+++ main.py
@@ -41,7 +41,8 @@ def get_hash(path_to_file):
 
 
 
-from database import add_video, get_videos, add_videos_from_csv
+from database import add_video, get_videos
+from read_csv import add_videos_from_csv
 # add_video(UUID=uuid.UUID('45e3ed7b-dc38-4717-8262-1fee5f8fb263'),
 #           upload_date='2024-07-30 00:45:36',
 #           content_hash=get_hash("https://s3.ritm.media/yappy-db-duplicates/45e3ed7b-dc38-4717-8262-1fee5f8fb263.mp4"),
@@ -58,8 +59,8 @@ def is_duplicate(videohashes):
             return (True, video.uuid)
     return (False, None)
 
-
-add_videos_from_csv()
+if __name__ == "__main__":
+    add_videos_from_csv()
 
 # testhash = hash_bytearray_to_hashes_array(get_hash("https://s3.ritm.media/yappy-db-duplicates/34502a8b-b45a-46e0-a3d6-a8561615f48b.mp4"))
 # print(is_duplicate(testhash))
--- read_csv.py
+++ read_csv.py
@@ -1,16 +1,19 @@
 import csv
 import uuid
 
-from database import add_video, session
+from database import add_video, close_session
 
-with open("train.csv", "r") as file:
-    reader = csv.reader(file)
-    count_row = list(reader)
-    for row in reader:
-        # print("uuid:" + row[1])
-        # print("upload_date:" + row[0])
-        # print("")
-        print(reader.line_num + f" из {count_row}")
-        add_video(UUID=uuid.UUID(row[1]), upload_date=row[0], is_duplicate=row[3], is_hard=row[5])
+def add_videos_from_csv():
+    with open("train.csv", "r") as file:
+        reader = csv.reader(file)
+        count_row = len(list(reader))
+        print("чтение из train.csv")
+        for row in reader:
 
-    session.close()
+            # print("uuid:" + row[1])
+            # print("upload_date:" + row[0])
+            # print("")
+            print(reader.line_num + f" из {count_row}")
+            add_video(UUID=uuid.UUID(row[1]), upload_date=row[0], is_duplicate=row[3], is_hard=row[5])
+
+        close_session()
