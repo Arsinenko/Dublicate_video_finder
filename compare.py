c9d4d04fe6dac9e6522d0b8ed88b553eda083de0
jxhrk <183056250+jxhrk@users.noreply.github.com>
2024-09-28 12:02:18 +0300

    cleanup

--- compare.py
+++ compare.py
@@ -1,25 +1,24 @@
 import imagehash
 
+def hashes_are_duplicates(hash1, hash2):
+    d = hash2-hash1
+    return d < 100 or d > 235
+
+
 def compare_hashes(data1, data2):
     count = 0
     for i1 in range(len(data1)):
         for i2 in range(len(data2)):
-            d = data2[i2]-data1[i1]
-            if (d < 100 or d > 235):
-                c = 0
-                while True:
-                    d = data2[i2]-data1[i1]
-                    if (d < 100 or d > 235):
-                        c+=1
-                        i1+=1
-                        i2+=1
-                        if (i1 >= len(data1) or i2 >= len(data2)):
-                            i1 = min(i1,len(data1)-1)
-                            i2 = min(i2,len(data2)-1)
-                            break
-                    else:
-                        break
-                count = max(count, c)
+            c = 0
+            while hashes_are_duplicates(data1[i1], data2[i2]):
+                c+=1
+                i1+=1
+                i2+=1
+                if (i1 >= len(data1) or i2 >= len(data2)):
+                    i1 = min(i1,len(data1)-1)
+                    i2 = min(i2,len(data2)-1)
+                    break
+            count = max(count, c)
     
     avg_frames = (len(data1)+len(data2))/2
     percent = count / avg_frames
