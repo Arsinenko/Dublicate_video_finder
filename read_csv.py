31772f9d167cf4584e3a7dacdf4a84b649e50b16
jxhrk <183056250+jxhrk@users.noreply.github.com>
2024-09-28 22:16:45 +0300

    Update read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -11,7 +11,7 @@ def add_videos_from_csv():
         reader = csv.reader(file)
         rows = list(reader)
         rows.pop(0)
-        
+
         count_row = len(rows)
 
         n = 1
@@ -19,9 +19,8 @@ def add_videos_from_csv():
         print(f"Чтение из {file_name}, кол-во строк: {count_row}")
         for row in rows:
             print(f"{n} из {count_row}")
-            print(f"uuid {row[1]}\nupload {row[0]}\ndupl {row[3]}\nhard {row[5]}")
-            break
-            #add_video(UUID=uuid.UUID(row[1]), upload_date=row[0], is_duplicate=row[3], is_hard=row[5])
-            #n++
+            #print(f"uuid {row[1]}\nupload {row[0]}\ndupl {row[3]}\nhard {row[5]}")
+            add_video(UUID=uuid.UUID(row[1]), upload_date=row[0], is_duplicate=row[3], is_hard=row[5])
+            n++
 
         close_session()
